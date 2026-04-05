85943aae25ee05e038e53710b04bb236a543710f
lunnlew <lunnlew@gmail.com>
2026-04-05 14:48:30 +0800

    feat: add Monaco editor worker setup for improved performance

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,15 @@ import { loadLocale, availableLocales } from './locales'
 
 loader.config({ monaco })
 
+self.MonacoEnvironment = {
+  getWorker: () => {
+    return new Worker(
+      new URL('./workers/monaco-editor-worker?worker', import.meta.url),
+      { type: 'module', name: 'monaco-editor-worker' }
+    )
+  },
+}
+
 // 预加载所有语言包，确保切换时不需要异步加载
 // 默认语言优先加载，确保首次渲染时即有对应语言文案
 Promise.all(availableLocales.map(({ code }) => loadLocale(code))).catch(() => {})
